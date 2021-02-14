2bdccbd0d1892d61dd7c3af8dbc3f4b13cba2697
AlekseiMakhov <66823049+AlekseiMakhov@users.noreply.github.com>
2021-02-14 14:46:42 +0300

    dev: added errors

--- errorTypes/ForbibbenError.js
+++
@@ -1,9 +0,0 @@
-class ForbiddenError extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = 403;
-    this.message = message;
-  }
-}
-
-module.exports = ForbiddenError;
